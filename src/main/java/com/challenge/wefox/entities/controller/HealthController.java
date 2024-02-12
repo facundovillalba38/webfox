61f7a0165d3b9255d3bda973d59c03d82c36221c
facundo.villalba <facundo.villalba@innopulse.es>
2024-02-12 17:29:59 +0100

    Add health controller

---
+++ src/main/java/com/challenge/wefox/entities/controller/HealthController.java
@@ -0,0 +1,15 @@
+package com.challenge.wefox.entities.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping
+    public String checkAppHealth(){
+        return "App is up and running!";
+    }
+}
